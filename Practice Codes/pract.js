53086ea709399f8a057932df26318c2c9ec27504
Kavita Shukla <kavita3689@gmail.com>
2024-10-03 10:43:39 +0530

    Kavita:Its just a Pract.

---
+++ Practice Codes/pract.js
@@ -0,0 +1,168 @@
+// console.log("Hello World",4+66, "Anthor log");
+// console.warn("This is warn");
+// console.error("This is an error")
+
+
+//variabl
+
+// var a=65;
+// var b=66;
+// c=(a+b);
+// d=(a-b);
+// e=(a*b);
+// f=(a/b);
+// console.log("This is add",c)
+// console.log("This is sub",d)
+// console.log("This is mul",e)
+// console.log("This is div",f)
+
+
+//datatypes
+//two types of data type 
+//primitive data type........num,str,booleans,undefine,null,symbol
+//reference data type........Arrays , object
+
+//nnumber
+// var num1 = 48;
+// var num2 = 48.55;
+// console.log(num1)
+
+//string
+var str1 = "HEllo World";
+var str2 = "Kavita";
+
+//object
+var marks = {
+  Kavita: 47,
+  Neha: 768,
+  Anamika: 476
+};
+// console.log(num1)
+// console.log(num2)
+// console.log(marks);
+// console.log(str1)
+// console.log(str2)
+
+//booleans
+
+var a = true;
+var b = false;
+// console.log(a,b);
+
+//undefine
+var und;
+// console.log(und);
+
+//null
+
+var n = null;
+// console.log(n);
+
+
+//arrays
+
+var arr = [1, 3, 5, 7, "Kavita", 4];
+// console.log(arr);
+
+//operators
+//Arithmatic opener...+,-,*,/
+
+var a = 76;
+var b = 37;
+// console.log("The Add of two no is",a+b);
+// console.log("The Add of two no is",a-b);
+// console.log("The Add of two no is",a*b);
+// console.log("The Add of two no is",a/b);
+
+
+
+//Assignment opr
+var c = b;
+c = c + 3;
+c -= 5;
+c *= 7;
+c /= 9;
+// console.log(c);
+
+// comparsion oprt
+
+var x = 67;
+var y = 77;
+// console.log(x==y);
+// console.log(x<y);
+// console.log(x>y);
+
+//logical oprt 
+//and
+// console.log(true&&true);
+// console.log(true&&false);
+// console.log(false&&true);
+// console.log(false&&false);
+
+//or
+// console.log(true||true);
+// console.log(true||false);
+// console.log(false||true);
+// console.log(false||false);
+
+
+//not
+// console.log(!false);
+// console.log(!true);
+
+
+
+//conditional statement
+
+// var age = 20;
+// if (age < 13) {
+//   console.log("You are a child.");
+
+// } else if (age < 18) {
+//   console.log("You are a teenager.");
+// } else {
+//   console.log("You are an adult."); 
+// }
+
+
+// //function
+
+// function avg(a,b){
+//      c=(a+b)/2
+//      return c;
+// }
+// c1= avg(74,54);
+// c2= avg(37,44);
+// c3= avg(67,33);
+// console.log(c1,c2 ,c3);
+
+
+
+// function add(a,b){
+//     c=a+b;
+//     return c;
+// }
+// d1=add(36,4);
+// console.log(d1);
+
+// function mul(a,b){
+//     c=a*b;
+//     return c;
+// }
+// d2=mul(3,4);
+// console.log(d2)
+
+
+//loops
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+for (var i = 0; i < arr.length; i++)
+
+  console.log(arr[i]);
+
+
+// j=0;
+// while(j<arr.length);{
+// console.log(arr[j]);
+// j++;
+// }
